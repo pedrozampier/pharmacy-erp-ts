c8347bf30a767bbec4063e10c026292e360eabda
pedrozampier <pedrootaviopz@gmail.com>
2024-11-19 10:35:55 -0300

    feat: add database connection configuration with MySQL and dotenv

---
+++ dist/config/database.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testConnection = testConnection;
+// /src/config/database.ts
+const mysql2_1 = __importDefault(require("mysql2"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const connection = mysql2_1.default.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+// Função para testar a conexão
+function testConnection() {
+    connection.connect((err) => {
+        if (err) {
+            console.error('Erro ao conectar no banco de dados:', err);
+            process.exit(1); // Encerra a aplicação em caso de erro
+        }
+        else {
+            console.log('Conectado ao banco de dados com sucesso!');
+        }
+    });
+}
+exports.default = connection;
