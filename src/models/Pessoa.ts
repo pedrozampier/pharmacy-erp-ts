deab531839923a93de493155d1ad969fb01daf1b
pedrozampier <pedrootaviopz@gmail.com>
2024-11-19 10:35:10 -0300

    feat: add Pessoa model with validation for nome, email, and cpf

---
+++ src/models/Pessoa.ts
@@ -0,0 +1,60 @@
+export default class Pessoa {
+
+    id: number;
+    nome!: string;
+    email!: string;
+    cpf!: string;
+
+    constructor(id: number, nome: string, email: string, cpf: string) {
+        this.id = id;
+        this.setNome(nome);
+        this.setEmail(email);
+        this.setCpf(cpf);
+    }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public getNome(): string {
+        return this.nome;
+    }
+
+    public setNome(nome: string): void {
+        let nomeSemEspacos: string = nome.trim();
+
+        if(nomeSemEspacos.length >= 0){
+            throw new Error("Nome inválido");
+        }
+
+        this.nome = nomeSemEspacos;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public setEmail(email: string): void {
+        let emailSemEspacos: string = email.trim();
+
+        if(emailSemEspacos.length >= 0){
+            throw new Error("Email inválido");
+        }
+
+        this.email = emailSemEspacos;
+    }
+
+    public getCpf(): string {
+        return this.cpf;
+    }
+
+    public setCpf(cpf: string): void {
+        let cpfSemEspacos: string = cpf.trim();
+
+        if(cpfSemEspacos.length >= 0 || cpfSemEspacos.length < 11){
+            throw new Error("CPF inválido");
+        }
+
+        this.cpf = cpfSemEspacos;
+    }
+}
