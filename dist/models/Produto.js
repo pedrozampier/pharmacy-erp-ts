76ae24bd4b4b45151f1c423cb00ea200ba1ecf60
pedrozampier <pedrootaviopz@gmail.com>
2024-11-28 00:10:52 -0300

    feat: refactor Produto class to extend Mercadoria and simplify constructor

--- dist/models/Produto.js
+++ dist/models/Produto.js
@@ -1,32 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Produto {
+const Mercadoria_1 = require("./Mercadoria");
+class Produto extends Mercadoria_1.Mercadoria {
     constructor(id, nome, preco) {
-        this.id = id;
-        this.setNome(nome);
-        this.setPreco(preco);
+        super(id, nome, preco);
     }
-    getId() {
-        return this.id;
-    }
-    getNome() {
-        return this.nome;
-    }
-    setNome(nome) {
-        let nomeSemEspacos = nome.trim();
-        if (nomeSemEspacos.length <= 0) {
-            throw new Error("Nome inválido");
-        }
-        this.nome = nomeSemEspacos;
-    }
-    getPreco() {
-        return this.preco;
-    }
-    setPreco(preco) {
-        if (preco <= 0) {
-            throw new Error("Preço inválido");
-        }
-        this.preco = preco;
+    descarte() {
+        console.log("Produto reciclavel, descarte em locais apropriados");
     }
 }
 exports.default = Produto;
--- src/models/Produto.ts
+++ src/models/Produto.ts
@@ -1,42 +1,12 @@
-export default class Produto {
+import { Mercadoria } from "./Mercadoria";
 
-    private id: number;
-    private nome!: string;
-    private preco!: number;
-
+export default class Produto extends Mercadoria {
+    
     constructor(id: number, nome: string, preco: number) {
-        this.id = id;
-        this.setNome(nome);
-        this.setPreco(preco);
+        super(id, nome, preco);
     }
 
-    public getId(): number {
-        return this.id;
-    }
-
-    public getNome(): string {
-        return this.nome;
-    }
-
-    public setNome(nome: string): void {
-        let nomeSemEspacos: string = nome.trim();
-
-        if(nomeSemEspacos.length <= 0){
-            throw new Error("Nome inválido");
-        }
-
-        this.nome = nomeSemEspacos;
-    }
-
-    public getPreco(): number {
-        return this.preco;
-    }
-
-    public setPreco(preco: number): void {
-        if(preco <= 0){
-            throw new Error("Preço inválido");
-        }
-
-        this.preco = preco;
+    public descarte(): void {
+        console.log("Produto reciclavel, descarte em locais apropriados");
     }
 }
