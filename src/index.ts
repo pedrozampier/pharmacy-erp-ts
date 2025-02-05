cbef240dc66abed229170c330c6b420c514915d5
pedrozampier <pedrootaviopz@gmail.com>
2025-02-05 09:34:10 -0300

    feat: update Remedio instantiation to use Tarja enum for the tarja property

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import Pessoa from "./models/Pessoa";
 import Produto from "./models/Produto";
 import Remedio from "./models/Remedio";
 import Venda from "./models/Venda";
+import { Tarja } from "./models/Tarja";
 
 //let pessoa = new Pessoa(1, "João", "joao@gmail.com", "12345678900");
 let cliente = new Cliente(1, "Jonas", "jonas@gmail.com", "12345678900", true, 1000);
 let farmaceutico = new Farmaceutico(1, "Joao", "joao@gmail.com", "12345678900", "234-0", 1500);
 
 let produto = new Produto(1, "Desodorante", 10);
-let remedio = new Remedio(1, "Dipirona", 5, false, "Não tarjado");
+let remedio = new Remedio(1, "Dipirona", 5, false, Tarja.PRETA);
 
 let date = new Date();
 date.setDate(date.getDate() - 1);
