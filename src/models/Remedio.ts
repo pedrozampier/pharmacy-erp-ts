36e8c2f75b20ec20009301de6639fe92afeb083f
pedrozampier <pedrootaviopz@gmail.com>
2025-02-05 09:33:48 -0300

    feat: update Remedio class to use Tarja enum for tarja property

--- src/models/Remedio.ts
+++ src/models/Remedio.ts
@@ -1,11 +1,12 @@
 import { Mercadoria } from "./Mercadoria";
+import { Tarja } from "./Tarja";
 
 export default class Remedio extends Mercadoria {
 
     private receitaObrigatoria: boolean;
-    private tarja: string;
+    private tarja: Tarja;
 
-    constructor(id: number, nome: string, preco: number, receitaObrigatoria: boolean, tarja: string) {
+    constructor(id: number, nome: string, preco: number, receitaObrigatoria: boolean, tarja: Tarja) {
         super(id, nome, preco);
         this.receitaObrigatoria = receitaObrigatoria;
         this.tarja = tarja;
